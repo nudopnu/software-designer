8ff0d486c6be22c7685c92f3a35e0738592bc02a
nudopnu <forsuse@live.de>
2024-08-29 14:52:41 +0200

    Fixed sidebar

--- client/src/src/app/components/sidebar/sidebar.component.ts
+++ client/src/src/app/components/sidebar/sidebar.component.ts
@@ -1,22 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'swd-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnDestroy{
   links = [
-    { label: 'Editor', link: '/documents', icon: 'form', selected: false },
+    { label: 'Editor', link: '/editor', icon: 'form', selected: false },
   ];
   collapsed = true;
+  subscription: Subscription;
 
   constructor(private router: Router) {
-    this.router.events.subscribe(event => {
+    this.subscription = this.router.events.subscribe(event => {
       if (event.type !== 1) { return; }
       this.links = this.links.map(item => ({ ...item, selected: event.url === item.link }))
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   navigate(link: string) {
