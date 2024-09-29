1578c62189d26e9ff11c412c0ba5d7dc5c4195f1
nudopnu <forsuse@live.de>
2024-09-29 20:19:38 +0200

    Added focus for node on creation

--- client/src/src/app/components/node/node.component.ts
+++ client/src/src/app/components/node/node.component.ts
@@ -1,24 +1,32 @@
-import { AfterContentInit, ChangeDetectorRef, Component, Input, QueryList, ViewChildren } from '@angular/core';
+import { AfterContentInit, AfterViewInit, ChangeDetectorRef, Component, input, Input, QueryList, ViewChild, viewChild, ViewChildren } from '@angular/core';
 import { Entity } from '../../models/data.model';
 import { NodeAttributeComponent } from '../node-attribute/node-attribute.component';
+import { NodeLabelComponent } from '../node-label/node-label.component';
 
 @Component({
   selector: 'swd-node',
   templateUrl: './node.component.html',
   styleUrl: './node.component.css'
 })
-export class NodeComponent implements AfterContentInit {
+export class NodeComponent implements AfterContentInit, AfterViewInit {
 
+  @ViewChild('title') titleComponent!: NodeLabelComponent;
   @ViewChildren('attributes') attributeComponents!: QueryList<NodeAttributeComponent>;
   @Input() hue = 210;
   @Input() entity: Entity = {
     name: "Entity",
     attributes: [],
   };
+  @Input() readonly = false;
 
   bgColor = '';
 
   constructor(private cdr: ChangeDetectorRef) { }
+
+  ngAfterViewInit(): void {
+    if (this.readonly) { return; }
+    this.titleComponent.focus();
+  }
 
   ngAfterContentInit(): void {
     this.bgColor = `radial-gradient(50% 90%, hsla(${this.hue}, 52%, 51%, 0.62) 0%, transparent 80%)`
