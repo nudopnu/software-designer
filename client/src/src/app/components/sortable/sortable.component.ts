8526c167cc61bc182da9fbb31281a12e65c40ea9
nudopnu <forsuse@live.de>
2024-10-01 18:19:55 +0200

    Added basic swapping mechanism

--- client/src/src/app/components/sortable/sortable.component.ts
+++ client/src/src/app/components/sortable/sortable.component.ts
@@ -1,22 +1,58 @@
-import { Component, Input, TemplateRef } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'swd-sortable',
   templateUrl: './sortable.component.html',
   styleUrl: './sortable.component.css'
 })
-export class SortableComponent<T> {
+export class SortableComponent<T> implements OnChanges {
 
   @Input() templateRef!: TemplateRef<any>;
   @Input() items: T[] = [];
 
-  onDragStart(event: DragEvent) {
-    console.log(event);
+  expanded: { before: boolean, after: boolean }[] = [];
+  draggedIdx = -1;
+  toExchangeIdx = -1;
+
+  constructor(private cdr: ChangeDetectorRef) { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!changes['items']) { return; }
+    this.expanded = this.items.map(item => ({
+      before: false,
+      after: false,
+    }));
+  }
+
+  onDragStart(event: DragEvent, index: number) {
+    console.log(event, index);
+    this.draggedIdx = index;
+    this.toExchangeIdx = index;
     const selection = window.getSelection();
-    if(selection) {
+    if (selection) {
       selection.removeAllRanges();
     }
     event.stopPropagation();
+  }
+
+  onDragEnd(event: DragEvent) {
+    const tmp = this.items[this.draggedIdx]
+    this.items[this.draggedIdx] = this.items[this.toExchangeIdx];
+    this.items[this.toExchangeIdx] = tmp;
+    this.expanded = this.items.map(item => ({
+      before: false,
+      after: false,
+    }));
+    this.cdr.detectChanges();
+  }
+
+  onDragOver(event: DragEvent, index: number) {
+    this.expanded = this.items.map((item, idx) => ({
+      before: this.draggedIdx > index && idx === index,
+      after: this.draggedIdx < index && idx === index,
+    }));
+    console.log(index, event);
+    this.toExchangeIdx = index;
   }
 
 }
